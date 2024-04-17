e99c3040793764cd4ef49e6e747d4fd2d87389f3
afarhadi99 <rosscohen1990@gmail.com>
2024-04-17 17:42:53 -0400

    added draw a ui

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,11 +1,35 @@
-import CustomerPortalForm from '@/components/ui/AccountForms/CustomerPortalForm';
-import EmailForm from '@/components/ui/AccountForms/EmailForm';
-import NameForm from '@/components/ui/AccountForms/NameForm';
 import Pricing from '@/components/ui/Pricing/Pricing'; // Import the Pricing component
 import { createClient } from '@/utils/supabase/server';
 
+import dynamic from "next/dynamic";
+import "@tldraw/tldraw/tldraw.css";
+import { useEditor } from "@tldraw/tldraw";
+import { getSvgAsImage } from "@/lib/getSvgAsImage";
+import { blobToBase64 } from "@/lib/blobToBase64";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { PreviewModal } from "@/components/PreviewModal";
+
+const Tldraw = dynamic(async () => (await import("@tldraw/tldraw")).Tldraw, {
+  ssr: false,
+});
+
 export default async function Account() {
+  const [html, setHtml] = useState<null | string>(null);
   const supabase = createClient();
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setHtml(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  });
 
   const {
     data: { user }
@@ -37,18 +61,85 @@ export default async function Account() {
 
   // Render the account page content for users with an active subscription
   return (
-    <section className="mb-32 bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
-        <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Private Page
-          </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Congrats! You have a subscription.
-          </p>
+    <>
+    <div className={`w-screen h-screen`}>
+      <Tldraw persistenceKey="tldraw">
+        <ExportButton setHtml={setHtml} />
+      </Tldraw>
+    </div>
+    {html &&
+      ReactDOM.createPortal(
+        <div
+          className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center"
+          style={{ zIndex: 2000, backgroundColor: "rgba(0,0,0,0.5)" }}
+          onClick={() => setHtml(null)}
+        >
+          <PreviewModal html={html} setHtml={setHtml} />
+        </div>,
+        document.body
+      )}
+  </>
+  );
+}
+
+function ExportButton({ setHtml }: { setHtml: (html: string) => void }) {
+  const editor = useEditor();
+  const [loading, setLoading] = useState(false);
+  // A tailwind styled button that is pinned to the bottom right of the screen
+  return (
+    <button
+      onClick={async (e) => {
+        setLoading(true);
+        try {
+          e.preventDefault();
+          const svg = await editor.getSvg(
+            Array.from(editor.currentPageShapeIds)
+          );
+          if (!svg) {
+            return;
+          }
+          const png = await getSvgAsImage(svg, {
+            type: "png",
+            quality: 1,
+            scale: 1,
+          });
+          const dataUrl = await blobToBase64(png!);
+          const resp = await fetch("/api/toHtml", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ image: dataUrl }),
+          });
+
+          const json = await resp.json();
+
+          if (json.error) {
+            alert("Error from open ai: " + JSON.stringify(json.error));
+            return;
+          }
+
+          const message = json.choices[0].message.content;
+          const start = message.indexOf("<!DOCTYPE html>");
+          const end = message.indexOf("</html>");
+          const html = message.slice(start, end + "</html>".length);
+          setHtml(html);
+        } finally {
+          setLoading(false);
+        }
+      }}
+      className="fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ="
+      style={{ zIndex: 1000 }}
+      disabled={loading}
+    >
+      {loading ? (
+        <div className="flex justify-center items-center ">
+          <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
         </div>
-      </div>
-    </section>
+      ) : (
+        "Make Real"
+      )}
+    </button>
   );
 }
 
